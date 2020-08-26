8eba0e4f1ff76e72672344d84899d42c7cfd9f1d
Clayton Herbst <herbsca1@gmail.com>
2020-08-26 22:17:01 +0800

    Started creating front-end web client

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket_contrib::helmet::{SpaceHelmet};
+use rocket_contrib::helmet::SpaceHelmet;
 use time_bank::routes;
 
 fn main() {
     rocket::ignite()
         .attach(SpaceHelmet::default())
-        .mount("/", routes![routes::welcome, routes::get_user_info])
+        .mount("/", routes![routes::welcome, routes::static_files])
+        .mount("/api/", routes![routes::get_user_info])
         .launch();
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,8 +1,21 @@
 use rocket_contrib::json::{JsonValue};
+use rocket::response::NamedFile;
+use std::path::{PathBuf, Path};
 
 #[get("/")]
-pub fn welcome() -> Option<&'static str> {
-  Some("Welcome to the time bank!")
+pub fn welcome() -> Option<NamedFile> {
+	NamedFile::open(Path::new("public/index.html")).ok()
+}
+
+#[get("/static/<file..>")]
+pub fn static_files(file: PathBuf) -> Option<NamedFile> {
+	let mut path_buf = file;
+	if path_buf.file_name() == None {
+		path_buf.set_file_name("index");
+		path_buf.set_extension("html");
+	}
+
+	NamedFile::open(Path::new("public/static/").join(path_buf)).ok()
 }
 
 #[get("/user")]
